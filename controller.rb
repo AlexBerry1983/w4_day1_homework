27df72e6cf3879e3c8e357a0dbe139a7d5c25e30
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-05-01 20:42:20 +0100

    link to welcome page

--- controller.rb
+++ controller.rb
@@ -3,6 +3,10 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('models/rps_game')
 
+get '/' do
+  erb(:welcome)
+end
+
 get '/result/:player1/:player2' do
   player1 = params[:player1]
   player2 = params[:player2]
